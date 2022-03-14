e8d2b1b6e93284612181cf0580be368952953d88
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-03-14 10:06:29 +0700

    membuat file component todolists inputusers

---
+++ src/InputUsers.js
@@ -0,0 +1,14 @@
+const InputUsers = () => {
+    return (
+        <>
+            <h2>Add item...</h2>
+            <input
+                type='text'
+                placeholder='Type item here...'
+            />
+            <button>add</button>
+        </>
+    );
+}
+ 
+export default InputUsers;
---
+++ src/TodoLists.js
@@ -0,0 +1,11 @@
+import InputUsers from "./InputUsers";
+
+const TodoLists = () => {
+    return (
+        <div className="todo-lists">
+            <InputUsers />
+        </div>
+    );
+}
+ 
+export default TodoLists;
